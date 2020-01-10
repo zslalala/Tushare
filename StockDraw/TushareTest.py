1c55685eb85b2b9301d09bc66c258a111bcbd71b
zs <1365105903@qq.com>
2020-01-10 22:28:45 +0800

    first version-2

--- StockDraw/TushareTest.py
+++ StockDraw/TushareTest.py
@@ -53,23 +53,24 @@ if __name__ == "__main__":
     pro = ts.pro_api(token)
 
     #输入参数，后续提到配置文件
-    ts_code = '000560'
-    start_date = '20180101'
-    end_date = '20191231'
+    ts_code = '601211'              #股票代码
+    start_date = '20171001'         #开始查询日期
+    end_date = '20200110'           #结束查询日期
 
+    #获取股票数据存储路径和上证/深证代码
     ts_code,path = getPathAndTC(ts_code)
-    print(ts_code)
 
+    #获取股票数据
     df = getStockData(path,start_date,end_date)
 
     #调整pandas数组的顺序
     order = ['trade_date','open','close','high','low','pre_close','change','pct_chg','vol','amount']
     df = df[order]
 
+    #绘图部分
     candleDf = df.as_matrix()
     num_time = date_to_num(candleDf[:,0])
     candleDf[:,0] = num_time
-
     fig, ax = plt.subplots(figsize=(20, 5))
     fig.subplots_adjust(bottom=0.1)
     mpf.candlestick_ochl(ax, candleDf, width=0.6, colorup='r', colordown='g', alpha=1.0)
@@ -82,15 +83,24 @@ if __name__ == "__main__":
     # x轴的刻度为日期
     ax.xaxis_date()
 
-    print(df.shape)
-    #上影线不能超过0.5%的收盘价
-    df1 = df[(df['high']<1.005*df['close'])]
-    #收盘价要大于1%的开盘价，防止收十字星
-    df2 = df1[df1['close']>1.005 * df1['open']]
-    #全天波动值要大于收盘开盘波动的两倍
-    df3 = df2[((df2['high']-df2['low'])>2 * (df2['close'] - df2['open']))]
-    print(df3.shape)
-    print("满足这样条件的天数:",df3.shape[0]/df.shape[0]*100,"%")
-    print(df3)
+    #股票分析部分
+    #要求一:该天是上涨的,至少上涨0.3%
+    df1 = df[df.close > 1.003 * df.open]
+    #要求二:上影线不能超过0.5%的收盘价
+    df1 = df1[df.high < 1.002 * df.close]
+    #要求三:下影线要超过两倍的箱体
+    df1 = df1[(df.open - df.low) > 2 * (df.close - df.open) ]
+    print(df1)
+
+
+    # #上影线不能超过0.5%的收盘价
+    # df1 = df[(df['high']<1.005*df['close'])]
+    # #收盘价要大于1%的开盘价，防止收十字星
+    # df2 = df1[df1['close']>1.005 * df1['open']]
+    # #全天波动值要大于收盘开盘波动的两倍
+    # df3 = df2[((df2['high']-df2['low'])>2 * (df2['close'] - df2['open']))]
+    # print(df3.shape)
+    # print("满足这样条件的天数:",df3.shape[0]/df.shape[0]*100,"%")
+    # print(df3)
 
     plt.show()
