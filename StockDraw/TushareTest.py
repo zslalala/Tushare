ccaad0f28c0bfe8cd00ba4cf7fd1e808a5ea92ea
zs <1365105903@qq.com>
2020-01-10 19:54:18 +0800

    Tushare Test OK

--- StockDraw/TushareTest.py
+++ StockDraw/TushareTest.py
@@ -2,12 +2,29 @@ import tushare as ts
 import matplotlib.pyplot as plt
 import matplotlib.finance as mpf
 from matplotlib.pylab import date2num
+import pandas as pd
 import datetime
+import os
+
+def getPathAndTC(ts_code):
+    ts_code = (str)(ts_code)
+    if ts_code[0] == '6':
+        path = 'D:\StockData\SH\SH'+ts_code+'.csv'
+        ts_code = ts_code + '.SH'
+        print(path)
+    elif (ts_code[0] == '3'or ts_code[0] == '0'):
+        path = 'D:\StockData\SZ\SZ'+ts_code+'.csv'
+        ts_code = ts_code + '.SZ'
+        print(path)
+    return ts_code,path
+
 
 def date_to_num(dates):
     num_time = []
     for date in dates:
+        #为什么int转换后会有.0这样的问题
         date = (str)(date)
+        date = date[:-2]
         date_time = datetime.datetime.strptime(date,'%Y%m%d')
         num_date = date2num(date_time)
         num_time.append(num_date)
@@ -15,12 +32,20 @@ def date_to_num(dates):
 
 if __name__ == "__main__":
     pro = ts.pro_api("8fef2d48965386e5b9f7517e0059dc3ea983b9c03214ab673be35442")
+    ts_code = '002024'
 
-    df_origin = pro.daily(ts_code='600398.SH', start_date='20190109', end_date='20200109')
+    ts_code,path = getPathAndTC(ts_code)
+    print(ts_code)
+
+    if(os.path.exists(path)):
+        print('true')
+        df_origin = pd.read_csv(path)
+    else:
+        print('false')
+        df_origin = pro.daily(ts_code=ts_code, start_date='20170101', end_date='20200110')
+        df_origin.to_csv(path,index=None)
+    # df_origin = pd.read_csv('D:\StockData\SH\SH600398.csv')
     df = df_origin.drop(['ts_code'],axis = 1)
-    print(df['trade_date'].dtype)
-
-    print(df[:3])
 
     order = ['trade_date','open','close','high','low','pre_close','change','pct_chg','vol','amount']
     df = df[order]
@@ -35,11 +60,12 @@ if __name__ == "__main__":
     plt.grid(True)
     # 设置日期刻度旋转的角度
     plt.xticks(rotation=30)
-    plt.title('600398')
+    plt.title(ts_code)
     plt.xlabel('Date')
     plt.ylabel('Price')
     # x轴的刻度为日期
     ax.xaxis_date()
+
 
     print(df.shape)
     #上影线不能超过0.5%的收盘价
--- StockDraw/YahooTest.py
+++ StockDraw/YahooTest.py
@@ -1,24 +1,8 @@
 # 导入需要的库
 import tushare as ts
-import matplotlib.pyplot as plt
-import matplotlib.finance as mpf
+
 
 if __name__ == "__main__":
-    # 设置历史数据区间
-    date1 = (2014, 12, 1)  # 起始日期，格式：(年，月，日)元组
-    date2 = (2016, 12, 1)  # 结束日期，格式：(年，月，日)元组
-    # 从雅虎财经中获取股票代码601558的历史行情
-    quotes = mpf.quotes_historical_yahoo_ohlc('601558.ss', date1, date2)
-
-    # 创建一个子图
-    fig, ax = plt.subplots(facecolor=(0.5, 0.5, 0.5))
-    fig.subplots_adjust(bottom=0.2)
-    # 设置X轴刻度为日期时间
-    ax.xaxis_date()
-    # X轴刻度文字倾斜45度
-    plt.xticks(rotation=45)
-    plt.title("股票代码：601558两年K线图")
-    plt.xlabel("时间")
-    plt.ylabel("股价（元）")
-    mpf.candlestick_ohlc(ax, quotes, width=1.2, colorup='r', colordown='green')
-    plt.grid(True)
+    pro = ts.pro_api("8fef2d48965386e5b9f7517e0059dc3ea983b9c03214ab673be35442")
+    df_origin = pro.daily(ts_code='002024.SZ', start_date='20170101', end_date='20200109')
+    print(df_origin)
